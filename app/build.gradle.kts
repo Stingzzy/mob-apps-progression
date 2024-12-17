17f75b4fdc70ce0c29d2e68ffb3b1d7b78e4842a
TheVlade <vladstepanskiy@gmail.com>
2024-12-17 18:57:53 +0100

    Credentials and Fragments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,12 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
